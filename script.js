721edda256995b4c263564909e09f97dcce5d7a2
VG Cargo Team <a.klassen@vgcargo.de>
2025-06-18 12:45:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Charter Dashboard Script – 3-spaltige strukturierte Detailansicht
 // Aktualisierte API_URL aus deinen letzten Uploads
-const API_URL = 'https://script.google.com/macros/s/AKfycbymbEsMiZrMeTTY1QU0DwhtVDepas_LZmjlToXtnHZFklFdf0qwWT_sfnEKu1eyzSkH/exec'; // <-- Überprüfen Sie, ob dies die aktuelle URL Ihrer bereitgestellten Web-App ist!
+const API_URL = 'https://script.google.com/macros/s/AKfycbzxlzbycoU7TvrNf4f-c_ruxcO5-U0IfsY1u0TDJv7r9Yw6fmqR-inMTvXxFGF11ma5/exec'; // <-- Überprüfen Sie, ob dies die aktuelle URL Ihrer bereitgestellten Web-App ist!
 
 let isAdmin = false; // Initialisiere isAdmin als false
 let requestData = []; // Speichert alle abgerufenen Charterdaten
