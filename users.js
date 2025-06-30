e91d34ea7e33dfca3549ab6310e6f622d1b40ed5
VG Cargo Team <a.klassen@vgcargo.de>
2025-06-30 07:19:31 +0200

    Update users.js

--- users.js
+++ users.js
@@ -5,9 +5,10 @@ export const users = {
   "dennis": { password: "vgcargo123", name: "Dennis Stscherbakow", role: "viewer" },
   "michael": { password: "vgcargo123", name: "Michael House", role: "viewer" },
   "slawa": { password: "vgcargo123", name: "Slawa Malik", role: "viewer" },
-  "geinrich": { password: "vgcargo123", name: "Heinrich Neufeld", role: "viewer" },
+  "heinrich": { password: "vgcargo123", name: "Heinrich Neufeld", role: "viewer" },
   "mike": { password: "vgcargo123", name: "Mike Becker", role: "viewer" },
   "benjamin": { password: "vgcargo123", name: "Benjamin Pankraz", role: "viewer" },
   "ines": { password: "vgcargo123", name: "Ines Kostuek", role: "viewer" },
-  "marcel": { password: "vgcargo123", name: "Marcel Fritsch", role: "viewer" }
+  "marcel": { password: "vgcargo123", name: "Marcel Fritsch", role: "viewer" },
+  "inge": { password: "vgcargo123", name: "Inge Huhn", role: "viewer" }
 };
